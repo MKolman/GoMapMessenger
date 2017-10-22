72108a39371e46e55a4cf7ac89cbd3f745951707
Maks Kolman <kolman.maks@gmail.com>
2017-10-22 19:21:12 +0200

    Add discord chat support and upgrade version

--- SECRETS.example.py
+++ SECRETS.example.py
@@ -1,25 +1,19 @@
 from fbchat import ThreadType
-# FB credentials
-USERNAME = "<FB username>"
-PASSWORD = "<FB password>"
 
+VERSION = 2.0
+
+# ============= GOMAP SETTINGS =============
 # Scanning bounding box
 LOCATION_W = "14.240820017805705"   # West most longitude
 LOCATION_E = "14.759138020524377"   # East most longitude
 LOCATION_N = "46.22983200641623"    # North most latitude
 LOCATION_S = "45.870103364048745"   # South most latitude
 
-
-CHAT = {"thread_id": "<Group chat ID>", "thread_type": ThreadType.GROUP}
-ADMIN = {"thread_id": "<Admin user ID>", "thread_type": ThreadType.USER}
-
-TIMEZONE = "Europe/Ljubljana"
-
 # All the pokemon you want to be informed about (by their pokedex number)
 POKEMON = [
-    147, 148, 149,  # Dratini
-    246, 247, 248,  # Larvitar
-    179, 180, 181,  # Mareep
+    147, 148, 149,  # Dratini family
+    246, 247, 248,  # Larvitar family
+    179, 180, 181,  # Mareep family
     143, 242, 131,  # Snorlax, Blissey, Lapras
     113, 176, 229,  # Chansey, Togetic, Houndoom
       3,   6,   9,  # Venosaur, Blastoise, Charizard
@@ -28,13 +22,61 @@ POKEMON = [
     130, 237, 241,  # Gyarados, Hitmontop, Miltank
     201,  94,  # Unown, Gengar
     115,  83, 128,  # Kangaskhan, Farfetch'd, Tauros
-    214, 222,  # Heracross Corsola
+    214, 222,  # Heracross, Corsola
 ]
 
-# Something like https://api.myjson.com/bins/2f4k
-GYM_JSON = None
-
+# Which raids do you want to report
 RAIDS = {
     "lvl": 5,  # Show all raids of lvl 5 and up
     "additional": [3, 248],  # Additionally show Venusaur and Tyranitar raids
 }
+
+
+# ============ CHAT SETTINGS ==============
+# Explicit timezone so the script works on servers in UTC
+TIMEZONE = "Europe/Ljubljana"
+
+
+CHAT = {
+    # Facebook messenger settings
+    "messenger": {
+        # Set this to True to enable FB messenger
+        "ACTIVATE": False,
+        # Enter your facebook username and password
+        "username": "<FB username>",
+        "password": "<FB password>",
+        # The main chat (you can get the id from the URL)
+        "chat": {
+            "thread_id": "<Group chat ID>",
+            "thread_type": ThreadType.GROUP
+        },
+        # You can use either another group, the same group or a user
+        "admin": {
+            "thread_id": "<Admin user ID>",
+            "thread_type": ThreadType.USER
+        },
+        # You can set a browser user agent to fake the browser you are using
+        # You can see your user agent here:
+        # https://www.whoishostingthis.com/tools/user-agent/
+        "user_agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "\
+                      "(KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36"
+    },
+
+    # Discord webhook
+    "discord": {
+        # Set this to True to enable Discord reporting
+        "ACTIVATE": False,
+        # Channel for pokemon sightings
+        "pokemon_webhook": "<webhook url>",
+        # Channel for raids
+        "raid_webhook": "<webhook url>",
+        # Imgur API client for image upload. Create one here:
+        # https://api.imgur.com/oauth2/addclient
+        "imgur_client_id": "<client ID>",
+    }
+}
+
+
+# ============ WALL OF SHAME ================
+# Something like https://api.myjson.com/bins/2f4k
+GYM_JSON = None
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import traceback
 import os
 import time
 
+import pyimgur
 from fbchat import Client
+from discord import Webhook
 
 from fetch import get_all_results
 from imager import make_image
@@ -18,30 +20,46 @@ def send_all():
     if len(all_results) == 0:
         print("No new results right now.")
         return
-    try:
-        print("Logging in...")
-        ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "\
-             "Gecko) Chrome/51.0.2704.106 Safari/537.36"
-        client = Client(SECRETS.USERNAME, SECRETS.PASSWORD, user_agent=ua)
-    except Exception as e:
-        print("Could not login!")
-        print(traceback.format_exc())
-        return False
-    try:
+
+    if SECRETS.CHAT['messenger']['ACTIVATE']:
+        sett = SECRETS.CHAT['messenger']
+        try:
+            print("Logging in...")
+            ua = sett['user_agent']
+            client = Client(sett['username'], sett['password'], user_agent=ua)
+        except Exception as e:
+            print("Could not login!")
+            print(traceback.format_exc())
+            return False
+        try:
+            for result in all_results:
+                print(result["message"])
+                make_image(result)
+                client.sendLocalImage("./img/tmp_raid.png", result["message"], **sett['chat'])
+        except Exception as e:
+            exc = traceback.format_exc()
+            print("GoMap Messenger failed")
+            print(exc)
+            client.sendMessage("GoMap Messenger sender failed. Sorry.", **sett['admin'])
+            client.sendMessage(exc, **sett['admin'])
+
+    elif SECRETS.CHAT['discord']['ACTIVATE']:
+        sett = SECRETS.CHAT['discord']
+        imgur = pyimgur.Imgur(sett['imgur_client_id'])
         for result in all_results:
-            print(result["message"])
             make_image(result)
-            # client.sendMessage(result["message"], **SECRETS.CHAT)
-            client.sendLocalImage("./img/tmp_raid.png", result["message"], **SECRETS.CHAT)
-    except Exception as e:
-        exc = traceback.format_exc()
-        print("GoMap Messenger failed")
-        print(exc)
-        client.sendMessage("GoMap Messenger sender failed. Sorry.", **SECRETS.ADMIN)
-        client.sendMessage(exc, **SECRETS.ADMIN)
+            img = imgur.upload_image("./img/tmp_raid.png")
+            if result['type'] == 'raid':
+                msg = Webhook(sett['raid_webhook'], msg=result['message'], image=img.link)
+            else:
+                msg = Webhook(sett['pokemon_webhook'], msg=result['message'], image=img.link)
+            msg.post()
 
 
 if __name__ == "__main__":
+    if not hasattr(SECRETS, "VERSION") or SECRETS.VERSION != "2.0":
+        print("You have to update your SECRETS.py to match those in SECRETS.example.py")
+        exit()
     os.environ['TZ'] = SECRETS.TIMEZONE
     if hasattr(time, "tzset"):
         time.tzset()
