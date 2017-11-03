2d0142bdc6add87f6b4ec4f151ad745c6297a42b
Maks Kolman <kolman.maks@gmail.com>
2017-11-03 09:00:57 +0100

    Add purge day to wall of shame
    Every third saturday is purge day when
    all rules about gyms standing 8:20 are off.

--- gyms.py
+++ gyms.py
@@ -1,6 +1,18 @@
-import SECRETS
-import requests
 import json
+from datetime import timedelta, date
+
+import requests
+
+import SECRETS
+
+
+def is_purge(dt):
+    """
+    Check if it is the third saturday in the month.
+    """
+    is_saturday = dt.weekday() == 5
+    is_third = (dt - timedelta(14)).month != (dt-timedelta(21)).month
+    return is_saturday and is_third
 
 
 def gym_update(gyms, filename="gym_owners.json"):
@@ -11,6 +23,8 @@ def gym_update(gyms, filename="gym_owners.json"):
             gym_owners = json.load(f)
     except FileNotFoundError as e:
         print("Creating new gym owners file")
+
+    purge = is_purge(date.today())
     for gym in gyms:
         owner = {
             "time": float("inf"),
@@ -32,9 +46,7 @@ def gym_update(gyms, filename="gym_owners.json"):
                 and owner["name"] is not None and \
                 gym_owners[gym_id]["team"] != owner["team"]:
             prev = gym_owners[gym_id]
-            # print("GYMS: Owner change for {}: from {} to {}".format(
-            #       gym_id, prev, owner))
-            if owner["time"] - prev["time"] < 500*60:
+            if purge and owner["time"] - prev["time"] < 500*60:
                 print("GYMS: ", owner, "is a bad boy", prev)
                 if owner['name'] not in gym_owners['shame']:
                     gym_owners['shame'][owner["name"]] = []
@@ -52,5 +64,16 @@ def gym_update(gyms, filename="gym_owners.json"):
 
 
 if __name__ == "__main__":
-    from tmp_data import gyms
-    gym_update(gyms)
+    # from tmp_data import gyms
+    # gym_update(gyms)
+    assert is_purge(date(2017, 10, 14)) is False
+    assert is_purge(date(2017, 10, 20)) is False
+    assert is_purge(date(2017, 10, 21)) is True
+    assert is_purge(date(2017, 10, 22)) is False
+    assert is_purge(date(2017, 10, 28)) is False
+
+    assert is_purge(date(2017, 11, 11)) is False
+    assert is_purge(date(2017, 11, 17)) is False
+    assert is_purge(date(2017, 11, 18)) is True
+    assert is_purge(date(2017, 11, 19)) is False
+    assert is_purge(date(2017, 11, 25)) is False
